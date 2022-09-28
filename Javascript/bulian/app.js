33f31dd72e512634487ebe6960db82a320ae2618
Marko <popovicmarko350@gmail.com>
2022-09-28 23:54:05 +0200

    Better scheduling documents

---
+++ Javascript/bulian/app.js
@@ -0,0 +1,10 @@
+broj1 = Number(prompt("Uesiti broj1"))
+broj2 = Number(prompt("Unesite broj2"))
+
+// if (broj1 === 1 && broj2 === 1) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+console.log(Boolean(broj1) && Boolean(broj2))
