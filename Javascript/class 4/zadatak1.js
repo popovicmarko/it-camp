9ecf59fc5eba6207a61c2c19cf6d4a725ec0b2f3
Marko <popovicmarko350@gmail.com>
2022-10-03 18:03:47 +0200

    class 4+

--- Javascript/class 4/zadatak1.js
+++ Javascript/class 4/zadatak1.js
@@ -1,9 +1,15 @@
-sabirac=0;
+// sabirac=0;
 
-for (let i = 0; i <= 20; i++) {
+// for (let i = 0; i <= 20; i++) {
  
-    if(i%2===0){
-        sabirac+=i;
-        console.log('Nakon broja', i, 'zbrij je', sabirac)
-    }
+//     if(i%2===0){
+//         sabirac+=i;
+//         console.log('Nakon broja', i, 'zbrij je', sabirac)
+//     }
+// }
+poruka="python"
+for(i=0; i<6; i++){
+    if(i===3)
+    poruka='je super'
+    console.log(poruka)
 }
