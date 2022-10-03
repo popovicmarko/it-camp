2cc9ebeb0b9ad4696d4331482d24a742c10725fd
Marko <popovicmarko350@gmail.com>
2022-10-03 16:45:12 +0200

    Uredjeno

---
+++ Javascript/class 1/homework1/app.js
@@ -0,0 +1,11 @@
+
+// ZADTAK 1
+
+
+a = Number(prompt("Uesiti broja"))
+b = Number(prompt("Unesite brojb"))
+if (a > 0 || b < a) {
+    console.log(true)
+} else {
+    console.log(false)
+}
---
+++ Javascript/class 1/homework1/app2.js
@@ -0,0 +1,16 @@
+
+// ZADATAK 2
+
+
+a = Number(prompt("Unesite broj A"))
+b = Number(prompt("Unesite broj B"))
+if (a > 0 && b < a) {
+    console.log(true)
+    if (a > b || b > 0) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+} else {
+    console.log(false)
+}
