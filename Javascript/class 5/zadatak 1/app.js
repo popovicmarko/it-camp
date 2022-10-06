3df45f2f7df76c17871f42582a73975b26082966
Marko <popovicmarko350@gmail.com>
2022-10-06 14:35:05 +0200

    Number guessing game

---
+++ Javascript/class 5/zadatak 1/app.js
@@ -0,0 +1,18 @@
+broj= new Number();
+brojZaTrazenje= Math.floor(Math.random()*100)
+while(true){
+    broj= Number (prompt("uneiste broj."))    
+    if(broj==0 || broj===brojZaTrazenje){
+        break;
+    }
+    if (broj < brojZaTrazenje){
+        alert('U uneite veci')
+        break;
+    }
+    if (broj > brojZaTrazenje){
+        alert("Unesite manji")
+    }
+}
+
+if(broj===brojZaTrazenje){}
+alert("Tacan broj")
