f384f0fdb81f33f9972e2fa7c39c31d284c6aef9
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2022-10-17 22:25:39 +0200

    funkcion class 1

---
+++ Javascript/class9/zadatak1/app.js
@@ -0,0 +1,44 @@
+
+
+function unosNiz(niz) {
+
+    
+        for ( let i = 0; i < 5; i++) {
+            clan = Number(prompt('Unesite elementa niza'))
+            niz.push(clan)
+        }
+    
+}
+ niz=[];
+
+
+
+function parnostNiz(niz) {
+    for (let i = 0; i <= niz.length; i++)
+        if (niz[i] % 2 === 0) {
+            console.log(niz[i])
+        }
+}
+
+function obrnutoNiz(niz) {
+    for (let i = niz.length; i >= 0; i--) {
+
+        console.log(niz[i])
+    }
+
+}
+
+function ispisNiz(niz) {
+    for (let i = 0; i < niz.length; i++) {
+
+        console.log(niz[i])
+    }
+
+}
+
+unosNiz(niz)
+
+obrnutoNiz(niz)
+
+parnostNiz(niz)
+
---
+++ Javascript/class9/zadatak2/app.js
@@ -0,0 +1,19 @@
+
+function Parnost(br1) {
+    if (br1 % 2 === 0) {
+        console.log("Funkcija je parna")
+    } else {
+        console.log("funkcijan nije parna")
+    }
+}
+
+function Negativnost(br2) {
+    if (br2 < 0) {
+        console.log("Broj je nagativan")
+    } else {
+        console.log("Broj je pozitivan")
+    }
+}
+
+Parnost(2)
+Negativnost(-2)
