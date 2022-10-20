3587835015008f69fc46f9a91d4e00a65ba45d66
Marko <popovicmarko350@gmail.com>
2022-10-20 23:55:28 +0200

    return

--- Javascript/class10/app.js
+++ Javascript/class10/app.js
@@ -1,1 +1,21 @@
 
+function Calc(nm1, nm2, operation) {
+    switch (operation) {
+        case '+':
+            return nm1 + nm2
+            break;
+        case '-':
+            return nm1 - nm2
+            break;
+        case '-':
+            return nm1 / nm2
+            break;
+        case '-':
+            return nm1 * nm2
+            break;
+        default:
+            return 'Error'
+    }
+}
+
+console.log(Calc(9, 5, '-'))
