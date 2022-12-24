94f41f4d9eb02bbed841c67236efb811730e60e2
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2022-12-24 18:28:57 +0100

    passing to boolean

--- Javascript/class 1/app.js
+++ Javascript/class 1/app.js
@@ -1,10 +1,6 @@
 broj1 = Number(prompt("Uesiti broj1"))
 broj2 = Number(prompt("Unesite broj2"))
 
-// if (broj1 === 1 && broj2 === 1) {
-//     console.log(true)
-// } else {
-//     console.log(false)
-// }
-
 console.log(Boolean(broj1) && Boolean(broj2))
+
+
