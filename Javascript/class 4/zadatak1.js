76c9f2ed7b628eda7ff2ace4548c52d9304c2ef7
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2022-12-30 19:27:10 +0100

    poyton is great * 5

--- Javascript/class 4/zadatak1.js
+++ Javascript/class 4/zadatak1.js
@@ -1,12 +1,4 @@
-// sabirac=0;
 
-// for (let i = 0; i <= 20; i++) {
- 
-//     if(i%2===0){
-//         sabirac+=i;
-//         console.log('Nakon broja', i, 'zbrij je', sabirac)
-//     }
-// }
 poruka="python"
 for(i=0; i<6; i++){
     if(i===3)
